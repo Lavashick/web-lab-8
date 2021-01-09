d35e14e194497ee034cfa1b04b3a64aad639d996
Lavashick <lavashick@icloud.com>
2021-01-10 01:56:44 +0300

    Задания 4 и 5

--- script.js
+++ script.js
@@ -50,3 +50,41 @@ for (let i = 0; i < matrixDimension; i++) {
     outputConsole(allString)
     allString = ""
 }
+
+outputConsole("\nЗАДАНИЕ 4")
+
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+let num1 = randomInteger(-150, 150)
+let num2 = randomInteger(-70, 70)
+let num3 = randomInteger(-36, 36)
+let arr = [num1, num2, num3]
+console.log("Исходные числа: ", arr)
+for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+            arr[j] += arr[j + 1];
+            arr[j + 1] = arr[j] - arr[j + 1];
+            arr[j] -= arr[j + 1];
+        }
+    }
+}
+console.log("Сортированные числа: ", arr)
+
+
+outputConsole("\nЗАДАНИЕ 5")
+let num4 = randomInteger(-150, 150)
+let num5 = randomInteger(-150, 150)
+let num6 = randomInteger(-150, 150)
+let num7 = randomInteger(-150, 150)
+let num8 = randomInteger(-150, 150)
+arr2 = [num4, num5, num6, num7, num8]
+console.log("Исходные числа: ", arr2)
+let maxNum = -151
+for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] > maxNum)
+        maxNum = arr2[i]
+}
+console.log("Максимальное число: ", maxNum)
