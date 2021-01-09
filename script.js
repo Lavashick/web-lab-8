a08ec45ce46188ad034a19849b63fa117da7cd18
Lavashick <lavashick@icloud.com>
2021-01-10 01:10:06 +0300

    Задание 1 и 2

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function outputConsole(text) {
     console.log(text)
 }
+
+outputConsole("ЗАДАНИЕ 1")
+let numSheep = prompt("Введите число овечек: ")
+for (let i = 1; i <= numSheep; i++) {
+    outputConsole(i + " овечка...")
+    if (i == numSheep) {
+        outputConsole("Овечки кончились :(")
+    }
+}
+
+
+outputConsole("\nЗАДАНИЕ 2")
+let start = 0
+let finish = 15
+for (let i = 0; i < finish; i++) {
+    if (i % 2 == 0) {
+        outputConsole(i + " чётное")
+    } else {
+        outputConsole(i + " нечётное")
+    }
+}
